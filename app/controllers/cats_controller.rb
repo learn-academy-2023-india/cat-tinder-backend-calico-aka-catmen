38b0cdb31798407e5c359ff844dd68baef658a31
irvinmoore <irvin.moore7@gmail.com>
2024-02-05 21:34:06 -0800

    Update to the Update Backend

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,16 +1,30 @@
 class CatsController < ApplicationController
-    before_action :set_cat, only: [:show, :update, :destroy]
-
     def index
-        cats = Cat.all
-        render json: cats 
+     cats = Cat.all
+     render json: cats
     end
-  
+ 
     def create
-        # Create a new cat
-        cat = Cat.create(cat_params)
-        render json: cat
+     cat = Cat.create(cat_params)
+     render json: cat
     end
-end
+ 
+    def update
+     cat = Cat.find(params[:id])
+     cat.update(cat_params)
+     render json: cat
+    end
+ 
+    def destroy
+     cat = Cat.find(params[:id])
+     cat.destroy
+     render json: cat
+    end
+ 
+    private
+    def cat_params
+     params.require(:cat).permit(:name, :age, :enjoys, :image)
+    end
+ end
 
     
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -5,13 +5,12 @@ RSpec.describe "Cats", type: :request do
     it "gets a list of cats" do
       Cat.create(
         name: 'Sir Whiskerton the Brave',
-        age: 2,
+        age: 2, 
         enjoys: 'Knighted for his bravery against vacuums, Sir Whiskerton seeks a tuna-loving hero with box-sitting prowess. Dislikes dogs, loves can-opening allies.',
         image: 'http://tinyurl.com/22357jc4'
       )
 
-      # Make a request
-      get '/cats'
+      get '/cats', as: :json
 
       cat = JSON.parse(response.body)
       expect(response).to have_http_status(200)
@@ -21,51 +20,63 @@ RSpec.describe "Cats", type: :request do
 
   describe "POST /create" do
     it "creates a cat" do
-      # The params we are going to send with the request
       cat_params = {
         cat: {
-          name: 'Buster',
-          age: 4,
-          enjoys: 'Meow Mix, and plenty of sunshine.',
-          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+          name: 'Captain Midnight',          
+          age: 5,
+          enjoys: 'Stealthy night prowler Captain Midnight seeks an adventurous partner for moonlit escapades. With a love for the shadows and midnight snacks, he\'s all about the thrill of the hunt under starry skies.',
+          image: 'http://tinyurl.com/3989srhn'
         }
       }
-  
-      # Send the request to the server
-      post '/cats', params: cat_params
-  
-      # Assure that we get a success back
-      expect(response).to have_http_status(200)
-  
-      # Look up the cat we expect to be created in the db
+
+      post '/cats', params: cat_params, as: :json
+
       cat = Cat.first
-  
-      # Assure that the created cat has the correct attributes
-      expect(cat.name).to eq 'Buster'
+      expect(response).to have_http_status(200)
+      expect(cat.name).to eq 'Captain Midnight'
     end
   end
 
-  describe "PATCH /cats/:id" do
-    let!(:cat) { Cat.create(name: 'Whiskers', age: 3, enjoys: 'Lounging in the sun', image: 'http://tinyurl.com/22357jc4') }
-  
+  describe "PUT /update" do
     it "updates a cat" do
-      new_attributes = {
+      cat = Cat.create(
+        name: 'Sir Whiskerton the Brave',
+        age: 2, 
+        enjoys: 'Knighted for his bravery against vacuums, Sir Whiskerton seeks a tuna-loving hero with box-sitting prowess. Dislikes dogs, loves can-opening allies.',
+        image: 'http://tinyurl.com/22357jc4'
+      )
+
+      cat_params = {
         cat: {
-          name: 'Whiskers II',
-          age: 4,
-          enjoys: 'Chasing shadows',
-          image: 'https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+          name: 'Sir Whiskerton the Brave',
+          age: 3,
+          enjoys: 'Knighted for his bravery against vacuums, Sir Whiskerton seeks a tuna-loving hero with box-sitting prowess. Dislikes dogs, loves can-opening allies.',
+          image: 'http://tinyurl.com/22357jc4'
         }
       }
-  
-      patch "/cats/#{cat.id}", params: new_attributes
-  
-      cat.reload
-      expect(cat.name).to eq 'Whiskers II'
-      expect(cat.age).to eq 4
-      expect(cat.enjoys).to eq 'Chasing shadows'
-      expect(cat.image).to eq new_attributes[:cat][:image]
+
+      put "/cats/#{cat.id}", params: cat_params, as: :json
+
+      cat = Cat.first
       expect(response).to have_http_status(200)
+      expect(cat.age).to eq 3
     end
   end
+
+  describe "DELETE /destroy" do
+    it "deletes a cat" do
+      cat = Cat.create(
+        name: 'Sir Whiskerton the Brave',
+        age: 2, 
+        enjoys: 'Knighted for his bravery against vacuums, Sir Whiskerton seeks a tuna-loving hero with box-sitting prowess. Dislikes dogs, loves can-opening allies.',
+        image: 'http://tinyurl.com/22357jc4'
+      )
+
+      delete "/cats/#{cat.id}"
+
+      cats = Cat.all
+      expect(response).to have_http_status(200)
+      expect(cats.length).to eq 0
+    end
+  end  
 end
