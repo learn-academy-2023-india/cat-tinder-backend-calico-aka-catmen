4add69c75c7b2346baa9768105420f499ece478f
irvinmoore <irvin.moore7@gmail.com>
2024-02-05 16:02:57 -0800

    EndPoints MVP completed

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,3 +1,25 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        render json: cats 
+    end
+  
+    def create
+        # Create a new cat
+        cat = Cat.create(cat_params)
+        render json: cat
+      end
+    
+      # Handle strong parameters, so we are secure
+      private
+      def cat_params
+        params.require(:cat).permit(:name, :age, :enjoys, :image)
+      end
+  
+    def update
+    end
+  
+    def destroy
+    end
    
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -2,6 +2,45 @@ require 'rails_helper'
 
 RSpec.describe "Cats", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of cats" do
+      Cat.create(
+        name: 'Sir Whiskerton the Brave',
+        age: 2,
+        enjoys: 'Knighted for his bravery against vacuums, Sir Whiskerton seeks a tuna-loving hero with box-sitting prowess. Dislikes dogs, loves can-opening allies.',
+        image: 'http://tinyurl.com/22357jc4'
+      )
+
+      # Make a request
+      get '/cats'
+
+      cat = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(cat.length).to eq 1
+    end
+  end
+  describe "POST /create" do
+    it "creates a cat" do
+      # The params we are going to send with the request
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix, and plenty of sunshine.',
+          image: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80'
+        }
+      }
+  
+      # Send the request to the server
+      post '/cats', params: cat_params
+  
+      # Assure that we get a success back
+      expect(response).to have_http_status(200)
+  
+      # Look up the cat we expect to be created in the db
+      cat = Cat.first
+  
+      # Assure that the created cat has the correct attributes
+      expect(cat.name).to eq 'Buster'
+    end
   end
 end
